1e36a8e99d05688968e81e78aa77e9746c9b08e9
hamigi <siddu9@users.noreply.huggingface.co>
2026-05-26 07:20:47 +0000

    Upload app.py with huggingface_hub

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore", message=".*torchvision.*")
 
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
